e10d2b9d84e1b7a8d4cdc5c8161eddc818ed03b0
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-08-20 21:14:13 -0400

    Decision Statement Cleanup

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -4,41 +4,44 @@
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1, 2 or 3: ");
             string userValue = Console.ReadLine();
 
+            string message = "";
+
             if (userValue == "1")
-            {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
-            }
-
+                message = "You won a new car!";
             else if (userValue == "2")
-            {
-                string message = "You won a new house!";
-                Console.WriteLine(message);
-            }
 
+                message = "You won a new house!";
             else if (userValue == "3")
-            {
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
-            }
-
+                message = "You won a new boat!";
             else if (userValue == "4")
-            {
-                string message = "You won a new plane!";
-                Console.WriteLine(message);
-            }
-
+                message = "You won a new plane!";
             else
-            {
-                string message = "Sorry, please try again!";
-                Console.WriteLine(message);
-            }
 
+            {
+                message = "Sorry, please try again!";
+                message += "You Lose.";
+            }
+            
+            Console.WriteLine(message);
             Console.ReadLine();
+            */
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            Console.Write("You won a ");
+            Console.Write(message);
+            Console.Write(".");
+            Console.ReadLine();
+
         }
     }
 }
